b636607a71eda8eabdfdf4904b7d291b42ad827b
Franrey Anthony S. Saycon <franreysaycon@gmail.com>
2021-10-02 22:58:07 +0800

    Simple left and right

--- src/ContributorDetails.js
+++ src/ContributorDetails.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { useLocation } from "react-router-dom"
+import { useLocation, useHistory } from "react-router-dom"
 
 const getCurrentSlug = (location) => {
     const paths = location.pathname.split("/")
@@ -13,17 +13,30 @@ const getCurrentSlug = (location) => {
 
 const ContributorDetails = ({ entries }) => {
     const location = useLocation()
+    const history = useHistory()
     const currentSlug = getCurrentSlug(location)
-    const currentEntry = location.pathname === "/" ? entries[0]: entries.find(entry => entry.slug === currentSlug)
+    
+    let currentEntryIndex = 0
+    if(!!currentSlug){
+        currentEntryIndex = entries.findIndex(entry => entry.slug === currentSlug)
+    }
+
+    const currentEntry = entries[currentEntryIndex]
+    const previous = currentEntryIndex > 0 ? entries[currentEntryIndex - 1] : null
+    const next = currentEntryIndex < entries.length - 1 ? entries[currentEntryIndex + 1] : null
 
     return (
-        currentEntry ?(
+        currentEntry ? (
         <div className="contributor">
-            <div className="content">
-                <h1>{currentEntry.title}</h1>
-                <div>{currentEntry.author}</div>
-                <a href={currentEntry.website} target="blank">{currentEntry.website}</a>
+            <div><span onClick={() => previous && history.push(`${previous.slug}`)} role="link">Previous</span></div>
+            <div className="contributor-description">
+                <div className="content">
+                    <h1>{currentEntry.title}</h1>
+                    <div>{currentEntry.author}</div>
+                    <a href={currentEntry.website} target="blank">{currentEntry.website}</a>
+                </div>
             </div>
+            <div><span onClick={() => next && history.push(`${next.slug}`)} role="link">Next</span></div>
         </div>
         ): null
     )
